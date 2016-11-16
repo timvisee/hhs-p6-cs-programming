155c7ad379b00c1c15fd0daaff7e560d511a5e12
Tim Visée <timvisee@gmail.com>
2016-11-16 23:18:04 +0100

    Create a base Program class

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hhs_p6_cs_programming {
+    public static class Program {
+
+        /// <summary>
+        /// Application name.
+        /// </summary>
+        private const string AppName = "HHS P6 C# Programming";
+
+        /// <summary>
+        /// Main method, to start the program.
+        /// </summary>
+        public static void Main() {
+            // Print the program name
+            Console.WriteLine(AppName);
+        }
+
+    }
+}
