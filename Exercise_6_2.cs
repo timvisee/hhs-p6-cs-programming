e4493ae97dac43a1fb4609fa67e52281571de2dc
Tim Visée <timvisee@gmail.com>
2016-11-16 22:35:24 +0100

    Complete exercise 6.2

--- Exercise_6_2.cs
+++ Exercise_6_2.cs
@@ -3,8 +3,105 @@
 namespace hhs_p6_cs_programming {
     public class Exercise_6_2 {
 
+        /// <summary>
+        /// Integer array this class works with.
+        /// </summary>
+        private static int[] _intArray;
+
+        /// <summary>
+        /// Main method, called on start.
+        /// </summary>
         public static void Main() {
-            Console.WriteLine("Hello, World!");
+            // Build the array and print it
+            BuildArray();
+            PrintArray("Initial array");
+
+            // Flip the first and last values and print it
+            FlipFirstLast();
+            PrintArray("A. Flip first & last");
+
+            // Rotate the array
+            Rotate();
+            PrintArray("B. Rotate");
+
+            // Zero even elements
+            ZeroEven();
+            PrintArray("C. Zero Even");
+
+            // Largest neighbor
+            LargestNeighbor();
+            PrintArray("D. Largest Neighbor");
+        }
+
+        /// <summary>
+        /// Build and fill the integer array.
+        /// </summary>
+        private static void BuildArray() {
+            // Create a new random object
+            Random rand = new Random();
+
+            // Create a new int array
+            _intArray = new int[8];
+
+            // Fill the array with random numbers
+            for(var i = 0; i < _intArray.Length; i++)
+                _intArray[i] = rand.Next(100);
+        }
+
+        /// <summary>
+        /// Print the current array in the console.
+        /// </summary>
+        private static void PrintArray(string message) {
+            // Print the message and the array to the console
+            Console.WriteLine("{0}:\n{1}\n", message, string.Join(", ", _intArray));
+        }
+
+        /// <summary>
+        /// Flip the first number in an integer array with the last.
+        /// </summary>
+        private static void FlipFirstLast() {
+            // Store the first variable
+            int first = _intArray[0];
+
+            // Flip the first and last values
+            _intArray[0] = _intArray[_intArray.Length - 1];
+            _intArray[_intArray.Length - 1] = first;
+        }
+
+        /// <summary>
+        /// Rotate the array.
+        /// </summary>
+        private static void Rotate() {
+            // Store the last value
+            int last = _intArray[_intArray.Length - 1];
+
+            // Shift all elements, starting with the last
+            for(var i = _intArray.Length - 1; i > 0; i--)
+                _intArray[i] = _intArray[i - 1];
+
+            // Set the first value to the original last value
+            _intArray[0] = last;
+        }
+
+        /// <summary>
+        /// Repalce all even elements with zero.
+        /// </summary>
+        private static void ZeroEven() {
+            // Loop thorugh the array
+            for(var i = 0; i < _intArray.Length; i++)
+                // Replace even elements with zero
+                if(_intArray[i] % 2 == 0)
+                    _intArray[i] = 0;
+        }
+
+        /// <summary>
+        /// Replace each element with the largest of it's two neighbors.
+        /// The first and last element are ignored.
+        /// </summary>
+        private static void LargestNeighbor() {
+            // Loop through the elements, and choose the largest neighbor
+            for(var i = 1; i < _intArray.Length - 1; i++)
+                _intArray[i] = Math.Max(_intArray[i - 1], _intArray[i + 1]);
         }
 
     }
