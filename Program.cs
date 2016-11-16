88fe8dd8bb6a78dc9b5a1a04d65346b7644cceba
Tim Visée <timvisee@gmail.com>
2016-11-16 23:32:26 +0100

    Add developer name

--- Program.cs
+++ Program.cs
@@ -9,11 +9,16 @@ namespace hhs_p6_cs_programming {
         private const string AppName = "HHS P6 C# Programming";
 
         /// <summary>
+        /// Developer name.
+        /// </summary>
+        private const string DeveloperName = "Tim Visee";
+
+        /// <summary>
         /// Main method, to start the program.
         /// </summary>
         public static void Main() {
             // Print the program name
-            Console.WriteLine("{0}\n", AppName);
+            Console.WriteLine("{0}\nDeveloped by {1}\n", AppName, DeveloperName);
 
             // Create and run an exercise
             new hhs_p6_cs_programming.exercises.p6.E2().Start();
