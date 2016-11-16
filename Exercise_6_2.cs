3cbb1a2478fffe9091bae44833caea9f4b03c726
Tim Visée <timvisee@gmail.com>
2016-11-16 22:05:41 +0100

    Initialize exercise 6.2, change project type

---
+++ Exercise_6_2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace hhs_p6_cs_programming {
+    public class Exercise_6_2 {
+
+        public static void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
