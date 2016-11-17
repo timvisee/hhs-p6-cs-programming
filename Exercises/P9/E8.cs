4d3993869dc5e11f2e3f5605ebda0b068b963336
Tim Visée <timvisee@gmail.com>
2016-11-17 17:44:08 +0100

    Add base for P9.8 exercise

---
+++ Exercises/P9/E8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hhs_p6_cs_programming.exercises.p9 {
+    public class E8 : BaseExercise {
+
+        protected override string ExerciseIdentifier { get { return "9.8"; } }
+
+        /// <summary>
+        /// Method to run the exercise.
+        /// </summary>
+        protected override void Run() {
+
+        }
+
+    }
+}
